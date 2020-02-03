e752047c533a30c69baef3f2b6bc381b794cd84c
ravichandrareddy88@gmail.com <ravichandrareddy88@gmail.com>
2020-02-03 11:50:19 +0530

    not done yet

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dict/uptown'));
+app.use(express.static('./dist/uptown'));
 
 app.get('/*', function(req,res) {
     
